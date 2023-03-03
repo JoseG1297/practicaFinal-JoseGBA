49aa5351db5fd79a59aa6983a4a5116f6fbd583f
JoseG Bautista <joseg.bautista@pjedomex.gob.mx>
2023-03-03 14:50:24 -0600

    configuraciones firebase parte 2

--- firebaseConfig/initialize.jsx
+++ firebaseConfig/initialize.jsx
@@ -1,16 +1,19 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAaMqbL7G8mJ2wa8-O6vHbCosl0Q_NyZ7E",
-  authDomain: "rc-practica7-auth.firebaseapp.com",
-  databaseURL: "https://rc-practica7-auth-default-rtdb.firebaseio.com",
-  projectId: "rc-practica7-auth",
-  storageBucket: "rc-practica7-auth.appspot.com",
-  messagingSenderId: "522575913642",
-  appId: "1:522575913642:web:bbd248a0f8c858cb0a66d3"
+  apiKey: "AIzaSyCDLkp4cCSpW1EdEhRwLNLGRAEgclmKUaU",
+  authDomain: "pictugram-josegba.firebaseapp.com",
+  projectId: "pictugram-josegba",
+  storageBucket: "pictugram-josegba.appspot.com",
+  messagingSenderId: "914328187859",
+  appId: "1:914328187859:web:a3f426ae584c71161bb526"
 };
-  
-  // Initialize Firebase
-  const appFirebase = initializeApp(firebaseConfig);
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
   
   export  {appFirebase};
