64db30c0845e018e749cebc2615951c5ed2558cd
JoseG Bautista <joseg.bautista@pjedomex.gob.mx>
2023-03-03 14:09:13 -0600

    configuraciones firebase

---
+++ firebaseConfig/database.jsx
@@ -0,0 +1,7 @@
+import {appFirebase} from "./initialize";
+import { getDatabase, ref, set, get, child, onValue } from "firebase/database";
+
+
+const database = getDatabase(appFirebase);
+
+export{ database, ref, set, get, child, onValue };
