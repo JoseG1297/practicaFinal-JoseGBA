bf7be3e2e8f542d42d3d78432b16947882675c81
Jose G. Bautista <mordo1309@gmail.com>
2023-03-04 22:13:13 -0600

    lista de post con imagenes

--- components/home/PostHome.jsx
+++ components/home/PostHome.jsx
@@ -1,37 +1,63 @@
-import { View, StyleSheet, Text, Image } from 'react-native';
+import { View, StyleSheet, Text, Image, FlatList } from 'react-native';
 import { Divider } from 'react-native-elements';
  
 export default function PostHome({postState}) {
   return (
     <View style={styles.container}>
         <Divider width={1} orientation='vertical'/>
-        <View style={styles.containerImages}> 
-            <View>
-                <Image source={{ uri: postState.usere_image }} style={styles.profilePost} />
-                <Text style={{ color: "white" }}>{postState.user_name}</Text>
+        <View style={styles.containerHeader}> 
+            <View style={styles.conatinerInfoHeader}>
+                <Image  source={{ uri: postState.user_image }} style={styles.profilePosts} />
+                <Text style={styles.profileText}>{postState.user_name}</Text>
             </View>
         </View>
+        <View style={styles.containerImagePost}>
+          <FlatList
+            horizontal={true} 
+            showsHorizontalScrollIndicator={false} 
+            data={postState.post_images}
+            renderItem={ ({ item, index }) => (
+              <Image source={{ uri: item.url }} key={index} style={{width: 450,  height: 450, resizeMode:'cover'}} />
+            )}
+          />
+          <Text style={styles.profileText}>{postState.user_name}</Text>
+          </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 9,
+    flex: 1,
     marginBottom: 30
   },
-  containerImages:{
+  containerHeader:{
     flexDirection: "row",
     justifyContent: "space-between",
     margin:5,
     alignItems: "center"
   },
+  conatinerInfoHeader:{
+    flexDirection: "row",
+    alignItems: "center"
+  },
+  containerImagePost:{
+    width: "100%",
+    height: 450
+  },
   profilePosts:{
-    width: 35,
-    height: 35,
+    width: 55,
+    height: 55,
     borderRadius: 50,
     marginLeft: 6,
-    borderBottomColor: 1.6,
-    borderColor: "#ffffff"
+    borderWidth: 3,
+    borderColor: "white",
+    resizeMode: 'contain'
+  },
+  profileText:{
+    color: "white",
+    fontSize: 16,
+    marginLeft: 10,
+    fontWeight: "bold",
   }
 });
--- components/screens/HomeScreen.jsx
+++ components/screens/HomeScreen.jsx
@@ -8,27 +8,67 @@ import PostHome from '../home/PostHome';
 const postData = [
     {
         user_id: 2,
-        user_image: 'https://pbs.twimg.com/profile_images/1222140802475773952/61OmyINj.jpg',
+        user_image: 'https://image.freepik.com/free-vector/mobile-wallpaper-with-fluid-shapes_79603-601.jpg',
         user_name: 'Test User DOS',
-        post_Caption: "Ques post tan amalon te acabas de aventar",
-        post_Images: [{ url:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjORKvjcbMRGYPR3QIs3MofoWkD4wHzRd_eg&usqp=CAU'}, {url:'https://files.oyebesmartest.com/uploads/preview/vivo-u20-mobile-wallpaper-full-hd-(1)qm6qyz9v60.jpg' }],
-        post_Coments: [{ user_name: 'usuarioUno', comment: 'esta padrisimo', user_name: 'usuarioDos', comment: 'esta aun mas padrisimo'}]
+        post_caption: "Ques post tan amalon te acabas de aventar",
+        post_images: [{ url:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjORKvjcbMRGYPR3QIs3MofoWkD4wHzRd_eg&usqp=CAU'}, {url:'https://files.oyebesmartest.com/uploads/preview/vivo-u20-mobile-wallpaper-full-hd-(1)qm6qyz9v60.jpg' }],
+        post_coments: [{ user_name: 'usuarioUno', comment: 'esta padrisimo', user_name: 'usuarioDos', comment: 'esta aun mas padrisimo'}]
     },
     {
         user_id: 1,
         user_image: 'https://pbs.twimg.com/profile_images/1222140802475773952/61OmyINj.jpg',
         user_name: 'Test User UNO',
-        post_Caption: "Ques post tan amalon te acabas de aventar",
-        post_Images: [{ url:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjORKvjcbMRGYPR3QIs3MofoWkD4wHzRd_eg&usqp=CAU'}, {url:'https://files.oyebesmartest.com/uploads/preview/vivo-u20-mobile-wallpaper-full-hd-(1)qm6qyz9v60.jpg' }],
-        post_Coments: [{ user_name: 'usuarioUno', comment: 'esta padrisimo', user_name: 'usuarioDos', comment: 'esta aun mas padrisimo'}]
+        post_caption: "Ques post tan amalon te acabas de aventar",
+        post_images: [{url:'https://files.oyebesmartest.com/uploads/preview/vivo-u20-mobile-wallpaper-full-hd-(1)qm6qyz9v60.jpg' }],
+        post_coments: [{ user_name: 'usuarioUno', comment: 'esta padrisimo', user_name: 'usuarioDos', comment: 'esta aun mas padrisimo'}]
     },
     {
         user_id: 3,
         user_image: 'https://pbs.twimg.com/profile_images/1222140802475773952/61OmyINj.jpg',
         user_name: 'Test User TRES',
-        post_Caption: "Ques post tan amalon te acabas de aventar",
-        post_Images: [{ url:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjORKvjcbMRGYPR3QIs3MofoWkD4wHzRd_eg&usqp=CAU'}, {url:'https://files.oyebesmartest.com/uploads/preview/vivo-u20-mobile-wallpaper-full-hd-(1)qm6qyz9v60.jpg' }],
-        post_Coments: [{ user_name: 'usuarioUno', comment: 'esta padrisimo', user_name: 'usuarioDos', comment: 'esta aun mas padrisimo'}]
+        post_caption: "Ques post tan amalon te acabas de aventar",
+        post_images: [{ url:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjORKvjcbMRGYPR3QIs3MofoWkD4wHzRd_eg&usqp=CAU'}, {url:'https://files.oyebesmartest.com/uploads/preview/vivo-u20-mobile-wallpaper-full-hd-(1)qm6qyz9v60.jpg' }],
+        post_coments: [{ user_name: 'usuarioUno', comment: 'esta padrisimo', user_name: 'usuarioDos', comment: 'esta aun mas padrisimo'}]
+    },
+    {
+        user_id: 9,
+        user_image: 'https://pbs.twimg.com/profile_images/1222140802475773952/61OmyINj.jpg',
+        user_name: 'Test User Nueve',
+        post_caption: "Ques post tan amalon te acabas de aventar",
+        post_images: [ {url:'https://files.oyebesmartest.com/uploads/preview/vivo-u20-mobile-wallpaper-full-hd-(1)qm6qyz9v60.jpg' }],
+        post_coments: [{ user_name: 'usuarioUno', comment: 'esta padrisimo', user_name: 'usuarioDos', comment: 'esta aun mas padrisimo'}]
+    },
+    {
+        user_id: 5,
+        user_image: 'https://pbs.twimg.com/profile_images/1222140802475773952/61OmyINj.jpg',
+        user_name: 'Test User CINCO',
+        post_caption: "Ques post tan amalon te acabas de aventar",
+        post_images: [{ url:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjORKvjcbMRGYPR3QIs3MofoWkD4wHzRd_eg&usqp=CAU'}, {url:'https://files.oyebesmartest.com/uploads/preview/vivo-u20-mobile-wallpaper-full-hd-(1)qm6qyz9v60.jpg' }],
+        post_coments: [{ user_name: 'usuarioUno', comment: 'esta padrisimo', user_name: 'usuarioDos', comment: 'esta aun mas padrisimo'}]
+    },
+    {
+        user_id: 6,
+        user_image: 'https://pbs.twimg.com/profile_images/1222140802475773952/61OmyINj.jpg',
+        user_name: 'Test User SEIS',
+        post_caption: "Ques post tan amalon te acabas de aventar",
+        post_images: [{ url:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjORKvjcbMRGYPR3QIs3MofoWkD4wHzRd_eg&usqp=CAU'}, {url:'https://files.oyebesmartest.com/uploads/preview/vivo-u20-mobile-wallpaper-full-hd-(1)qm6qyz9v60.jpg' }],
+        post_coments: [{ user_name: 'usuarioUno', comment: 'esta padrisimo', user_name: 'usuarioDos', comment: 'esta aun mas padrisimo'}]
+    },
+    {
+        user_id: 7,
+        user_image: 'https://pbs.twimg.com/profile_images/1222140802475773952/61OmyINj.jpg',
+        user_name: 'Test User SIETE',
+        post_caption: "Ques post tan amalon te acabas de aventar",
+        post_images: [{ url:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjORKvjcbMRGYPR3QIs3MofoWkD4wHzRd_eg&usqp=CAU'}, {url:'https://files.oyebesmartest.com/uploads/preview/vivo-u20-mobile-wallpaper-full-hd-(1)qm6qyz9v60.jpg' }],
+        post_coments: [{ user_name: 'usuarioUno', comment: 'esta padrisimo', user_name: 'usuarioDos', comment: 'esta aun mas padrisimo'}]
+    },
+    {
+        user_id: 8,
+        user_image: 'https://pbs.twimg.com/profile_images/1222140802475773952/61OmyINj.jpg',
+        user_name: 'Test User OCHO',
+        post_caption: "Ques post tan amalon te acabas de aventar",
+        post_images: [{ url:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjORKvjcbMRGYPR3QIs3MofoWkD4wHzRd_eg&usqp=CAU'}, {url:'https://files.oyebesmartest.com/uploads/preview/vivo-u20-mobile-wallpaper-full-hd-(1)qm6qyz9v60.jpg' }],
+        post_coments: [{ user_name: 'usuarioUno', comment: 'esta padrisimo', user_name: 'usuarioDos', comment: 'esta aun mas padrisimo'}]
     }
 ]
 
@@ -40,7 +80,7 @@ export default function HomeScreen() {
       <StoriesHome />
       <ScrollView>
         {postData.map((post, index) =>(
-            <PostHome postState={post}  key={post.user_id}/>
+            <PostHome postState={post}  key={index}/>
         ))}
       </ScrollView>
     </SafeAreaView>
@@ -49,10 +89,6 @@ export default function HomeScreen() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    flexDirection: 'column'
-  },
-  containerPost:{
     flex: 1,
   }
 });
