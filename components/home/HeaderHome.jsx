b7047ee33a5dc8af5ecb32bb8994a316e94dde13
Jose G. Bautista <mordo1309@gmail.com>
2023-03-04 18:23:10 -0600

    Primera Version Encabezado

--- components/home/HeaderHome.jsx
+++ components/home/HeaderHome.jsx
@@ -1,15 +1,45 @@
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Image, TouchableOpacity, Text} from 'react-native';
+
+import { AntDesign } from '@expo/vector-icons'; 
+import { Ionicons } from '@expo/vector-icons'; 
 
 export default function HeaderHome() {
   return (
     <View style={styles.container}>
-      <Image source={require('../../imagenes/LogoWhite.png')} />
+      <TouchableOpacity>
+        <Image style={styles.logo} source={require('../../imagenes/LogoWhite.png')} />
+      </TouchableOpacity>
+      <View style={styles.iconosContainer}>
+        <TouchableOpacity>
+          <AntDesign style={styles.icons} name="pluscircleo" size={30} color="white" />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <AntDesign style={styles.icons} name="smileo" size={30} color="white" />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Ionicons style={styles.icons} name="chatbubble-outline" size={30} color="white" />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 2,
+  logo:{
+    width:150,
+    height:75,
+    resizeMode: 'contain'
   },
+  container: {
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    flexDirection: 'row',
+    marginHorizontal: 10
+  },
+  iconosContainer:{
+    flexDirection: 'row',
+  },
+  icons:{
+    marginLeft: 10,
+  }
 });
