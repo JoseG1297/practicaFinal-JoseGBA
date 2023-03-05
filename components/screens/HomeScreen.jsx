74f2288de750b14d806c782b380f914c2e602a47
Jose G. Bautista <mordo1309@gmail.com>
2023-03-04 19:49:25 -0600

    Insta Stories V1

--- components/screens/HomeScreen.jsx
+++ components/screens/HomeScreen.jsx
@@ -1,11 +1,13 @@
 import { SafeAreaView, StyleSheet, Text } from 'react-native';
 
 import HeaderHome from '../home/HeaderHome';
+import StoriesHome from '../home/StoriesHome';
 
 export default function HomeScreen() {
   return (
     <SafeAreaView style={styles.container}>
       <HeaderHome />
+      <StoriesHome />
     </SafeAreaView>
   );
 }
