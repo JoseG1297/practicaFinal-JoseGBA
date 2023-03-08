c389ec53a4ba919fe6f9969dd9e2aab3e9c724d5
Jose G. Bautista <mordo1309@gmail.com>
2023-03-07 20:53:41 -0600

    nueva navegacion General

--- components/navegacion/NavegacionGeneral.jsx
+++ components/navegacion/NavegacionGeneral.jsx
@@ -4,15 +4,19 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import "react-native-gesture-handler"; 
 
+import LoginScreen from '../screens/LoginScreen';
+import RegistroScreen from '../screens/RegistroScreen';
 import BioScreen from '../screens/BioScreen';
 import HomeScreen from '../screens/HomeScreen';
 
 const Stack = createNativeStackNavigator();
 
-export default function NavegacionInterna() {
+export default function NavegacionAuth() {
   return (
     <NavigationContainer >
-      <Stack.Navigator initialRouteName="Home" >
+      <Stack.Navigator initialRouteName="Registro" >
+        <Stack.Screen name="Login" component={LoginScreen} options={{headerShown: false}}   />
+        <Stack.Screen name="Registro" component={RegistroScreen} options={{headerShown: false}} />
         <Stack.Screen name="Home" component={HomeScreen} options={{headerShown: false}}   />
         <Stack.Screen name="Biografia" component={BioScreen} options={{headerShown: false}}  />
       </Stack.Navigator>
